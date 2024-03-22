f0fb83c4fb56ab4c1e59191eb9f050e132691c56
Matti Henriksen <henrim3@rpi.edu>
2024-03-22 13:20:16 -0400

    update find highlighter

--- neovim/.config/nvim/lua/plugins/find-highlighter.lua
+++ neovim/.config/nvim/lua/plugins/find-highlighter.lua
@@ -1,7 +1,6 @@
 return {
     "henrim3/nvim-find-highlighter",
-    dir = "/home/matti/code/projects/nvim-find-highlighter",
-    dev = false,
+    -- dir = "/home/matti/code/projects/nvim-find-highlighter",
     config = function()
         require("nvim-find-highlighter").setup()
     end
