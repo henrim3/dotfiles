52645a9fe6da83c3ebb7c764a8dcb2efe96c8906
Matti Henriksen <henrim3@rpi.edu>
2024-03-27 22:19:00 -0400

    a

--- neovim/.config/nvim/lua/plugins/lsp.lua
+++ neovim/.config/nvim/lua/plugins/lsp.lua
@@ -7,7 +7,7 @@ vim.api.nvim_create_autocmd('LspAttach', {
         vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
         vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
         vim.keymap.set({ 'n', 'i' }, '<c-i>', vim.lsp.buf.hover, opts)
-        vim.keymap.set({ 'n', 'i' }, '<c-o>', vim.lsp.buf.signature_help, opts)
+        vim.keymap.set({ 'n', 'i' }, '<c-e>', vim.lsp.buf.signature_help, opts)
         vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
         vim.keymap.set('n', '<leader>D', vim.lsp.buf.type_definition, opts)
         vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts)
@@ -60,11 +60,14 @@ return {
                             }
                         }
                     }}
-                    lspconfig.clangd.setup ({
+                end,
+                ["clangd"] = function()
+                    local lspconfig = require("lspconfig")
+                        lspconfig.clangd.setup ({
                         cmd = {
                             "clangd",
                             "--offset-encoding=utf-16",
-                        }
+                        },
                     })
                 end,
             },
