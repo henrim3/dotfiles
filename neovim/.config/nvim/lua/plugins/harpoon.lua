88e2b55412b3de999abcec316dedc5cd1728d580
Matti Henriksen <henrim3@rpi.edu>
2024-05-05 17:06:05 -0400

    wider harpoon menu

--- neovim/.config/nvim/lua/plugins/harpoon.lua
+++ neovim/.config/nvim/lua/plugins/harpoon.lua
@@ -3,20 +3,26 @@ return {
     dependencies = {
         "nvim-lua/plenary.nvim",
     },
-    config = function ()
-        vim.keymap.set("n", "<leader>hm", function ()
+    config = function()
+        require("harpoon").setup({
+            menu = {
+                width = vim.api.nvim_win_get_width(0) - 4,
+            }
+        })
+
+        vim.keymap.set("n", "<leader>hm", function()
             require("harpoon.mark").add_file()
         end)
 
-        vim.keymap.set("n", "<leader>a", function ()
+        vim.keymap.set("n", "<leader>a", function()
             require("harpoon.ui").toggle_quick_menu()
         end)
 
-        vim.keymap.set("n", "<leader>;", function ()
+        vim.keymap.set("n", "<leader>;", function()
             require("harpoon.ui").nav_next()
         end)
 
-        vim.keymap.set("n", "<leader>,", function ()
+        vim.keymap.set("n", "<leader>,", function()
             require("harpoon.ui").nav_prev()
         end)
     end
