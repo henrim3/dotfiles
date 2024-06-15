208a1d2700a8549844c41f46fe4668df42d7b668
Matti Henriksen <henrim3@rpi.edu>
2024-06-14 22:03:46 -0400

    unmap aerial function jumping

--- neovim/.config/nvim/lua/plugins/aerial.lua
+++ neovim/.config/nvim/lua/plugins/aerial.lua
@@ -4,8 +4,8 @@ return {
         require("aerial").setup({
             on_attach = function(bufnr)
                 -- Jump forwards/backwards with '{' and '}'
-                vim.keymap.set("n", "{", "<cmd>AerialPrev<CR>", { buffer = bufnr })
-                vim.keymap.set("n", "}", "<cmd>AerialNext<CR>", { buffer = bufnr })
+                -- vim.keymap.set("n", "{", "<cmd>AerialPrev<CR>", { buffer = bufnr })
+                -- vim.keymap.set("n", "}", "<cmd>AerialNext<CR>", { buffer = bufnr })
             end,
         })
     end,
